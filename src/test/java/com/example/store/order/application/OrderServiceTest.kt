1163c2295cbf8a87212b4cf02aa21476d8d589b8
igno <cruzsuarezbrandon@gmail.com>
2025-04-21 23:48:32 -0500

    test: add unit test for saveOrder method to handle non-existent product ID

--- src/test/java/com/example/store/order/application/OrderServiceTest.kt
+++ src/test/java/com/example/store/order/application/OrderServiceTest.kt
@@ -1,19 +1,20 @@
 package com.example.store.order.application
 
 import com.example.store.category.domain.Category
+import com.example.store.order.domain.Order
+import com.example.store.order.domain.OrderDetails
 import com.example.store.order.domain.OrderRepository
-import com.example.store.order.infrastructure.dto.OrderDTO
-import com.example.store.order.infrastructure.dto.OrderDetailsDTO
-import com.example.store.order.infrastructure.mapper.OrderMapper
 import com.example.store.product.domain.Product
 import com.example.store.product.domain.ProductRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import kotlin.test.assertEquals
 
 @ExtendWith(MockitoExtension::class)
 class OrderServiceTest {
@@ -28,34 +29,29 @@ class OrderServiceTest {
     @InjectMocks
     private lateinit var orderService: OrderServiceImpl
 
-    private lateinit var orderMapper: OrderMapper
-
     @BeforeEach
     fun setUp() {
 
     }
 
-//    @Test
-//    fun saveOrderShouldReturnOrder() {
-//        // Given
-//        val orderDetailsDTO = listOf(
-//            OrderDetailsDTO(1, 10),
-//            OrderDetailsDTO(2, 15),
-//        )
-//
-//        val orderDTO = OrderDTO(orderDetailsDTO)
-//
-//        val order = orderMapper.OrderDTOToOrder(orderDTO)
-//
-//        Mockito.`when`(productRepository.findAllById(listOf(1L,2L))).thenReturn(listOf(
-//            Product(1, "Product 1", "Description 1", 10.0, 10, Category(1, "Category 1"), null),
-//            Product(2, "Product 2", "Description 2", 20.0, 15, Category(2, "Category 2"), null)))
-//
-//        Mockito.`when`(orderRepository.save(order)).thenReturn(order)
-//        // When
-//        orderService.save(order)
-//
-//        // Then
-//        // Verify that the order and order details are saved correctly
-//    }
+    @Test
+    fun saveOrderWhenProductIdNoExistShouldReturnThrowIllegalArgumentException() {
+
+        //These are the data that should come from the controller
+        val order = Order.builder()
+            .orderDetails(listOf(
+                OrderDetails.builder().product(Product.builder().id(1).build()).quantity(10).build(),
+                OrderDetails.builder().product(Product.builder().id(2).build()).quantity(15).build()))
+            .build()
+
+        Mockito.`when`(productRepository.findAllById(listOf(1L,2L))).thenReturn(listOf(
+            Product(1, "Product 1", "Description 1", 10.0, 10, Category(1, "Category 1"), null),
+            ))
+
+        val exception = assertThrows<IllegalArgumentException> {
+            orderService.save(order)
+        }
+        assertEquals("Product no encontrado", exception.message)
+
+    }
 }
